171c9bca5e5e6f6ef60de87b7623308140439794
Your Name <you@example.com>
2022-03-17 15:59:03 +0100

    class Opera

---
+++ class/Opera.php
@@ -0,0 +1,49 @@
+<?php
+
+class Opera {
+    private $provenienza;
+    private $titolo;
+    private $autore;
+    private $data;
+    private $immagine;
+
+    function getTitolo(){
+        return $this->titolo;
+    }
+
+    function setTitolo(string $titolo){
+        $this->titolo = $titolo; 
+    }
+
+    function getAutore(){
+        return $this->autore;
+    }
+
+    function setAutore(string $autore){
+        $this->autore = $autore; 
+    }
+
+    function getData(){
+        return $this->data;
+    }
+
+    function setData(string $data){
+        $this->data = $data; 
+    }
+
+    function getProvenienza(){
+        return $this->provenienza;
+    }
+
+    function setProvenienza(string $provenienza){
+        $this->provenienza = $provenienza; 
+    }
+
+    function getImmagine(){
+        return $this->immagine;
+    }
+
+    function setImmagine(string $immagine){
+        $this->immagine = $immagine; 
+    }
+}
