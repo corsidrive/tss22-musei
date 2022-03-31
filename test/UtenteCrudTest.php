867351af75e355f444b5c10ffc087919ed912c42
Your Name <you@example.com>
2022-03-31 12:18:10 +0200

    tolto user id

--- test/UtenteCrudTest.php
+++ test/UtenteCrudTest.php
@@ -6,3 +6,13 @@ $utenteCrud = new UtenteCrud;
 $risultato = $utenteCrud->readAll();
 
 print_r($risultato);
+echo "-------------------------------<br>";
+$utente = new Utente();
+$utente->setNome("Arrivo");
+$utente->setCognome("Da Database");
+
+
+$utenteCrud->create($utente);
+$risultato = $utenteCrud->readAll();
+
+print_r($risultato);
