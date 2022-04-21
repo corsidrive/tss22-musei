997ee1f23cafb3f3f08bea6f61568e1d5249d70b
Your Name <you@example.com>
2022-04-21 09:22:28 +0200

    validazione nome

--- add_user_controller.php
+++ add_user_controller.php
@@ -9,26 +9,65 @@ $email = "";
 $emailInvalid = false;
 $emailErrore = "";
 
-if($_SERVER['REQUEST_METHOD'] === 'GET'){
+$nome = "";
+$nomeInvalid = false;
+$nomeErrore = "";
+
+$formIsValid = true;
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // echo "appena arrivato";
 
 }
 
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
-    // print_r($_POST);
-    // controllare i dati
-    $test = filter_input(INPUT_POST,"email",FILTER_VALIDATE_EMAIL);
-    
-    if($test === false) {
+    $email = $_POST['email'];
+    $test = filter_var($email, FILTER_VALIDATE_EMAIL);
+
+    if ($test === false) {
         $emailErrore = "la mail è obbligatoria";
-        $emailInvalid = true; 
-    }else{
-        $email = $test;
+        $emailInvalid = true;
+        $formIsValid = false;
     }
 
+    var_dump($nome);
+    var_dump(strlen($nome));
+    var_dump(strlen($nome) === 0);
+ 
+    if (strlen($nome) === 0) {
+        $nomeErrore = "il nome è obbligatorio";
+        $nomeInvalid = true;
+        $formIsValid = false;
+       echo "entro";
+        die();
+    }
+
+    if ($formIsValid) {
+
+
+        var_dump($_POST);
+
+        $userCrud = new UtenteCrud();
+        $utente = new Utente();
+        $utente->setNome(filter_input(INPUT_POST, 'nome', FILTER_SANITIZE_SPECIAL_CHARS));
+        $utente->setCognome(filter_input(INPUT_POST, 'cognome', FILTER_SANITIZE_SPECIAL_CHARS));
+
+        $userCrud->create($utente);
+
+        header('location: rigaziamenti.php');
+    }
 }
 
 
-$view = new AddUserView($nomeSezione,$nomeCategoria,$email,$emailErrore,$emailInvalid); 
+$view = new AddUserView(
+    $nomeSezione,
+    $nomeCategoria,
+    $email,
+    $emailErrore,
+    $emailInvalid,
+    $nome,
+    $nomeErrore,
+    $nomeInvalid
+);
 $view->render();
--- view/AddUserView.php
+++ view/AddUserView.php
@@ -1,76 +1,81 @@
 <?php
-class AddUserView {
+class AddUserView
+{
     private $nomeSezione;
     private $nomeCategoria;
     private $emailErrore;
     private $emailInvalid;
     private $email;
 
-    public function __construct($nomeSezione
-                                ,$nomeCategoria
-                                ,$email
-                                ,$emailErrore
-                                ,$emailInvalid
-                                ) 
-                                {
+    public function __construct(
+        $nomeSezione,
+        $nomeCategoria,
+        $email,
+        $emailErrore,
+        $emailInvalid,
+        $nome,
+        $nomeErrore,
+        $nomeInvalid
+    ) {
         $this->nomeSezione = $nomeSezione;
         $this->nomeCategoria = $nomeCategoria;
+
         $this->email = $email;
         $this->emailErrore = $emailErrore;
         $this->emailInvalid = $emailInvalid;
-       
+
+        $this->nome = $nome;
+        $this->nomeErrore = $nomeErrore;
+        $this->nomeInvalid = $nomeInvalid;
 
         // print_r($this);
     }
 
     public function render()
-    { 
+    {
         //print_r($this->nomeSezione);
         include "view/layout/head.php";
         include "view/layout/header.php";
         include "view/layout/sidebar.php";
-        
-        print_r(
-            $_SERVER['REQUEST_METHOD']
-        );
-        
-        
-        ?>
+
+      
+
+?>
 
         <form action="add_user_controller.php" method="POST">
 
-        <div class="mb-3">
-            <label class="form-label">Nome</label>
-            <input class="form-control"
-                   name="nome" 
-                   type="text"
-                   
-                   >
-            <div class="invalid-feedback">
-                il nome è obbligatorio
+            <div class="mb-3">
+                <label class="form-label">Cognome</label>
+
+                <input class="form-control" name="cognome" type="text">
+                <div class="invalid-feedback">
+                     
+                </div>
+
             </div>
-         
-        </div>
-        <div class="mb-3">
-            <label class="form-label">Cognome</label>
-            <input class="form-control"
+            <div class="mb-3">
+                <label class="form-label">Nome</label>
 
-                   name="cognome" 
-                   type="text" >
-                   
-        </div>
-        <div class="mb-3">
-            <label class="form-label">email</label>
-            <?php $emailClass = $this->emailInvalid ? "is-invalid" : "is-valid"  ; ?>
-            <input class="form-control <?= $emailClass ?>"
-                   name="email" 
-                   value="<?= $this->email ?>"
-                   type="text" 
-                   >
-                   
-        </div>
+                <?php $nomeClass = $_SERVER['REQUEST_METHOD'] === 'POST' ? ($this->nomeInvalid ? "is-invalid" : "is-valid") : ""; ?>
 
-        <!-- <div class="mb-3">
+                <input class="form-control   <?= $nomeClass ?>" 
+                name="nome"
+                value="<?= $this->nome ?>"
+                type="text">
+
+                <div class="invalid-feedback">
+                <?= $this->nomeErrore ?>
+                </div>
+            </div>
+
+            <div class="mb-3">
+                <label class="form-label">email</label>
+                <?php $emailClass = $_SERVER['REQUEST_METHOD'] === 'POST' ? ($this->emailInvalid ? "is-invalid" : "is-valid") : ""; ?>
+                <input class="form-control <?= $emailClass ?>" name="email" value="<?= $this->email ?>" type="text">
+
+            </div>
+
+            <!-- <div class="mb-3">
             <label class="form-label">eta</label>
             <input class="form-control"
                  max="100"
@@ -81,14 +86,14 @@ class AddUserView {
                    
         </div> -->
 
-        
 
-        <div class="mb-3">
-            <button class="btn btn-primary" type="submit">Aggiungi utente</button>
-        </div>
+
+            <div class="mb-3">
+                <button class="btn btn-primary" type="submit">Aggiungi utente</button>
+            </div>
         </form>
 
-        <?php
+<?php
         include "view/layout/footer.php";
     }
-} 
+}
